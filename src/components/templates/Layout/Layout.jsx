a8d3575a86422a68a244282ddd7b93f023f6a34b
Taump <taump@yandex.ru>
2023-07-28 20:01:00 +0500

    fix: link order

--- src/components/templates/Layout/Layout.jsx
+++ src/components/templates/Layout/Layout.jsx
@@ -19,12 +19,12 @@ export const Layout = ({ children }) => {
 			<div className="text-center text-white/60 mt-[-30px] pb-2 font-light">
 
 				<div className="flex flex-wrap justify-center mb-5 space-x-5">
-					<a className="p-2 hover:opacity-70" href="https://discord.obyte.org/" target="_blank" rel="noreferrer"><img src="/social_media/discord.svg" alt="Obyte discord" /></a>
+					<a className="p-2 hover:opacity-70" href="https://x.com/ObyteOrg" target="_blank" rel="noreferrer"><img src="/social_media/x.svg" alt="Obyte X (Twitter)" /></a>
 					<a className="p-2 hover:opacity-70" href="https://t.me/obyteorg" target="_blank" rel="noreferrer"><img src="/social_media/telegram.svg" alt="Obyte telegram" /></a>
-					<a className="p-2 hover:opacity-70" href="https://mp.weixin.qq.com/s/JB0_MlK6w--D6pO5zPHAQQ" target="_blank" rel="noreferrer"><img src="/social_media/weixin.svg" alt="Obyte weixin" /></a>
-					<a className="p-2 hover:opacity-70" href="https://x.com/ObyteOrg" target="_blank" rel="noreferrer"><img src="/social_media/x.svg" alt="Obyte x (Twitter)" /></a>
-					<a className="p-2 hover:opacity-70" href="https://www.youtube.com/channel/UC59w9bmROOeUFakVvhMepPQ/" target="_blank" rel="noreferrer"><img src="/social_media/youtube.svg" alt="Obyte YouTube" /></a>
+					<a className="p-2 hover:opacity-70" href="https://discord.obyte.org/" target="_blank" rel="noreferrer"><img src="/social_media/discord.svg" alt="Obyte discord" /></a>
 					<a className="p-2 hover:opacity-70" href="https://blog.obyte.org/" target="_blank" rel="noreferrer"><img src="/social_media/medium.svg" alt="Obyte medium" /></a>
+					<a className="p-2 hover:opacity-70" href="https://mp.weixin.qq.com/s/JB0_MlK6w--D6pO5zPHAQQ" target="_blank" rel="noreferrer"><img src="/social_media/weixin.svg" alt="Obyte weixin" /></a>
+					<a className="p-2 hover:opacity-70" href="https://www.youtube.com/channel/UC59w9bmROOeUFakVvhMepPQ/" target="_blank" rel="noreferrer"><img src="/social_media/youtube.svg" alt="Obyte YouTube" /></a>
 					<a className="p-2 hover:opacity-70" href="https://www.reddit.com/r/obyte/" target="_blank" rel="noreferrer"><img src="/social_media/reddit.svg" alt="Obyte reddit" /></a>
 					<a className="p-2 hover:opacity-70" href="https://bitcointalk.org/index.php?topic=1608859.0" target="_blank" rel="noreferrer"><img src="/social_media/bitcointalk.svg" alt="Obyte bitcointalk" /></a>
 					<a className="p-2 hover:opacity-70" href="https://www.facebook.com/obyte.org" target="_blank" rel="noreferrer"><img src="/social_media/facebook.svg" alt="Obyte facebook" /></a>
