3d09783630c96a2685f58462366d3b34c46f8b95
Taump <taump@yandex.ru>
2023-08-03 13:21:41 +0500

    fix: remove go to main

--- src/components/templates/PageLayout/PageLayout.jsx
+++ src/components/templates/PageLayout/PageLayout.jsx
@@ -4,7 +4,7 @@ import { ArrowLeftIcon } from "@/components/icons";
 
 export const PageLayout = ({ children, description, title = "No title" }) => (
 	<div className="max-w-5xl p-4 mx-auto mt-8 border-2 md:p-6 lg:p-10 bg-gray-950 rounded-3xl border-primary/30">
-		<Link
+		{/* <Link
 			to="/"
 			className="inline-flex items-center mb-3 space-x-2 font-medium cursor-pointer lg:mb-5"
 		>
@@ -15,11 +15,12 @@ export const PageLayout = ({ children, description, title = "No title" }) => (
 				/>
 			</div>
 			<div className="text-primary/70">Go to main</div>
-		</Link>
+		</Link> */}
 
-		<h1 className="mb-3 text-3xl font-bold text-gray-300 lg:mb-5 lg:text-5xl">
+		<h1 className="mb-3 text-3xl font-bold text-gray-300 lg:mb-6 lg:text-5xl">
 			{title}
 		</h1>
+
 		{!!description && (
 			<h3 className="text-gray-400 text-md lg:text-lg">{description}</h3>
 		)}
